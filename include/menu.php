76e74816e76c65cb251241a0a1f0dbdd9b09f6db
ali-aljarrah <ali.jarrah.109@gmail.com>
2025-01-03 19:38:11 +0800

    first commit

---
+++ include/menu.php
@@ -0,0 +1,57 @@
+<header class="header-area fadeInDown" data-wow-delay="0.2s">
+        <div class="classy-nav-container light breakpoint-off">
+            <div class="container">
+                <!-- Classy Menu -->
+                <nav class="classy-navbar light justify-content-between" id="dreamNav">
+
+                    <!-- Logo -->
+                    <a class="nav-brand light" href="#"><img src="img/core-img/logo.png" alt="logo"> Robostam </a>
+
+                    <!-- Navbar Toggler -->
+                    <div class="classy-navbar-toggler demo">
+                        <span class="navbarToggler"><span></span><span></span><span></span></span>
+                    </div>
+
+                    <!-- Menu -->
+                    <div class="classy-menu">
+
+                        <!-- close btn -->
+                        <div class="classycloseIcon">
+                            <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                        </div>
+
+                        <!-- Nav Start -->
+                        <div class="classynav">
+                            <ul id="nav">
+                                <li><a href="#home">Home</a>
+                                    <ul class="dropdown">
+                                        <li><a href="index-demo-1.html">Home style 1</a></li>
+                                        <li><a href="index-demo-2.html">Home style 2</a></li>
+                                        <li><a href="index-demo-3.html">Home style 3</a></li>
+                                        <li><a href="index-demo-4.html">Home style 4</a></li>
+                                        <li><a href="index-demo-5.html">Home style 5</a></li>
+                                        <li><a href="index-demo-6.html">Home style 6</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="about-us.html">About Us</a></li>
+                                <li><a href="services.html">Services</a></li>
+                                <li><a href="faq.html">FAQ</a></li>
+                                <li><a href="#blog">Blog</a>
+                                    <ul class="dropdown">
+                                        <li><a href="index-blog.html">Blog Posts</a></li>
+                                        <li><a href="index-blog-with-sidebar.html">Sidebar Blog </a></li>
+                                        <li><a href="index-single-blog.html">Blog Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact-us.html">Contact</a></li>
+                            </ul>
+
+                            <!-- Button -->
+                            <a href="#" class="btn login-btn ml-50">Log in</a>
+                        </div>
+                        <!-- Nav End -->
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php include './include/head.php'; ?>
+
+    <!-- Title -->
+    <title>Multipurpose Template</title>
+
+</head>
+
+<body class="light-version">
+    <!-- Preloader -->
+    <?php include './include/loader.php'; ?>
+
+    <!-- ##### Header Area Start ##### -->
+    <?php include './include/menu.php'; ?>
+    <!-- ##### Header Area End ##### -->
+
+    <section class="hero-section ico-bg2 relative hidden" id="home">
+        <div class="ico-circled"></div>
+        <!-- Hero Content -->
+        <div class="hero-section-content">
+            
+            <div class="container ">
+                <div class="row align-items-center">
+                    <!-- Welcome Content -->
+                    <div class="col-12 col-lg-6 col-md-12">
+                        <div class="welcome-content text-left">
+                            <div class="promo-section">
+                                <h3 class="special-head gradient-text cyan mt-s">Creative Artificial Intelligence Solutions</h3>
+                            </div>
+                            <div class="cd-intro v2 text-left">
+                                <h1 class="cd-headline clip is-full-width thin" >
+                                    <span class="w-text bold">AI Data Analysis helps grow faster & </span>
+                                    <span class="cd-words-wrapper">
+                                        <b class="gradient-text cyan bolder is-hidden">Manage like A Pro<span class="o-text">.</span></b>
+                                        <b class="gradient-text cyan bolder is-visible">Scale to Success<span class="o-text ">.</span></b>
+                                    </span>
+                                </h1>
+                            </div>
+                            <p class="g-text fadeInUp" data-wow-delay="0.3s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit quaerat est, a labore excepturi rem sed eius facere error! Dolore in perspiciatis porro dolor debitis</p>
+                            <div class="dream-btn-group fadeInUp w-text" data-wow-delay="0.4s">
+                                <a href="#" class="btn more-btn pink mr-3">Learn More</a>
+                                <div class="video-demo-prev">
+                                    <a href="#" class="btn more-btn video-btn v2  mr-3"><i class="fa fa-play"></i> </a>
+                                    <span>Watch Demo</span>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="mt-50 col-12 col-lg-6 offset-lg-0 col-md-10 offset-md-1 col-sm-12 ">
+                        <div class="fadeInUp" style="" data-wow-delay="0.5s">
+                            <img src="img/core-img/animated-img1.gif" alt="" class="spec-ml city-img img-responsive center-block">
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <?php include './include/footer.php'; ?>
+
+</body>
+
+</html>
