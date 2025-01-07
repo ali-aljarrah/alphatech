de491f4b70152ab3b10b6be3fb243148ea518875
ali-aljarrah <ali.jarrah.109@gmail.com>
2025-01-07 19:00:50 +0800

    create pages

---
+++ WLAN_solution.php
@@ -0,0 +1,52 @@
+<?php include './include/head.php'; ?>
+
+    <!-- Title -->
+    <title>Alphatech - WLAN Solution</title>
+
+</head>
+
+<body class="light-version">
+    <!-- Preloader -->
+    <?php include './include/loader.php'; ?>
+
+    <!-- ##### Header Area Start ##### -->
+    <?php include './include/menu.php'; ?>
+    <!-- ##### Header Area End ##### -->
+
+    <!-- ##### Welcome Area Start ##### -->
+    <div class="breadcumb-area clearfix dzsparallaxer auto-init" data-options='{direction: "normal"}'>
+        <div class="divimage dzsparallaxer--target"></div>
+        <!-- breadcumb content -->
+        <div class="breadcumb-content">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center">
+                    <div class="col-12">
+                        <nav aria-label="breadcrumb" class="breadcumb--con text-center">
+                            <h2 class="w-text title wow fadeInUp" data-wow-delay="0.2s">WLAN Solution</h2>
+                            <ol class="breadcrumb justify-content-center wow fadeInUp" data-wow-delay="0.4s">
+                                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                                <li class="breadcrumb-item"><a href="#">Solutions</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">WLAN Solution</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Welcome Area End ##### -->
+
+    <!-- ##### About Us Area Start ##### -->
+    <section class="about-us-area ico-about-bg section-padding-100-0 clearfix" id="about">
+        <div class="container mb-5">
+         
+        </div>
+    </section>
+
+
+    <?php include './include/solutions.php'; ?>
+    <?php include './include/footer.php'; ?>
+
+</body>
+
+</html>
---
+++ telecom_network_optimization.php
@@ -0,0 +1,71 @@
+<?php include './include/head.php'; ?>
+
+    <!-- Title -->
+    <title>Alphatech - Telecom Network Optimization</title>
+
+</head>
+
+<body class="light-version">
+    <!-- Preloader -->
+    <?php include './include/loader.php'; ?>
+
+    <!-- ##### Header Area Start ##### -->
+    <?php include './include/menu.php'; ?>
+    <!-- ##### Header Area End ##### -->
+
+    <!-- ##### Welcome Area Start ##### -->
+    <div class="breadcumb-area clearfix dzsparallaxer auto-init" data-options='{direction: "normal"}'>
+        <div class="divimage dzsparallaxer--target"></div>
+        <!-- breadcumb content -->
+        <div class="breadcumb-content">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center">
+                    <div class="col-12">
+                        <nav aria-label="breadcrumb" class="breadcumb--con text-center">
+                            <h2 class="w-text title wow fadeInUp" data-wow-delay="0.2s">Telecom Network Optimization</h2>
+                            <ol class="breadcrumb justify-content-center wow fadeInUp" data-wow-delay="0.4s">
+                                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                                <li class="breadcrumb-item"><a href="#">Solutions</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Telecom Network Optimization</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Welcome Area End ##### -->
+
+    <!-- ##### About Us Area Start ##### -->
+    <section class="about-us-area ico-about-bg section-padding-100-0 clearfix" id="about">
+        <div class="container mb-5">
+            <div class="row align-items-center">
+                <div class="row align-items-center">
+                    <div class="col-12 col-lg-6 offset-lg-0 mb-4 mb-md-0">
+                        <div class="who-we-contant wow fadeInLeft" data-wow-delay="0.3s">
+                            <h4>Telecom Network Optimization</h4>
+                            <p>
+                            Our services involve rollout or swap engineering optimization, daily KPI optimization, network O&M, with deep understanding of multi-technology wireless network, our optimization is based on a broad range of experiences of planning & design, which leads to great improvement of utilization of network resources and QOS as well.
+                            </p>
+                            <p>
+                            Our engineers expertise in Huawei, ZTE, Nokia Siemens , over 200000 BTS optimized by us across China, Indonesia and Malaysia.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 offset-lg-0 col-md-12 no-padding-left">
+                        <div class="wow fadeInRight" data-wow-delay="0.3s">
+                            <img loading="lazy" class="img-fluid" src="img/alphatec/img-11.webp" alt="Alphatech - Telecom Network Optimization">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <?php include './include/solutions.php'; ?>
+    <?php include './include/footer.php'; ?>
+
+</body>
+
+</html>
